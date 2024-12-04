8c529d48bd47a4230fddb8813ddf03363f72da72
mohak-golani <mohakgolani09@gmail.com>
2024-12-05 02:24:01 +0530

    updated index.ts and added new files

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
-import { server as WebSocketServer } from "websocket";
+import { OutgoingMessage, SupportedMessage as OutgoingSupportedMessages } from "./messages/outgoingMessages";
+import { connection, server as WebSocketServer } from "websocket";
 import http from 'http';
+import { UserManager } from "./UserManager";
+import { IncomingMessage, SupportedMessage } from "./messages/incomingMessage";
+import { InMemoryStore } from "./store/InMemoryStore";
 
-var server = http.createServer(function(request: any, response: any) {
+const server = http.createServer(function(request: any, response: any) {
     console.log((new Date()) + ' Received request for ' + request.url);
     response.writeHead(404);
     response.end();
 });
+const userManager = new UserManager();
+const store = new InMemoryStore();
+
+
 server.listen(8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
 
 const wsServer = new WebSocketServer({
     httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
     autoAcceptConnections: false
 });
 
 function originIsAllowed(origin: string) {
-  // put logic here to detect whether the specified origin is allowed.
+
   return true;
 }
 wsServer.on('request', function(request) {
@@ -36,8 +39,13 @@ wsServer.on('request', function(request) {
     console.log((new Date()) + ' Connection accepted.');
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+            try{
+                messageHandler(connection, JSON.parse(message.utf8Data));
+            } catch(e) {
+
+            }
+            // console.log('Received Message: ' + message.utf8Data);
+            // connection.sendUTF(message.utf8Data);
         }
         else if (message.type === 'binary') {
             console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
@@ -48,3 +56,37 @@ wsServer.on('request', function(request) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
+
+function messageHandler(ws: connection, message: IncomingMessage) {
+    if (message.type == SupportedMessage.JoinRoom){
+        const payload = message.payload;
+        userManager.addUser(payload.name,payload.userId, payload.roomId, ws);
+    }
+    if ( message.type === SupportedMessage.SendMessage) 
+    {
+        const payload = message.payload;
+        const user = userManager.getUser(payload.roomId, payload.userId);
+        if (!user ) {
+            console.error("user not found in the db");
+            return;
+        }
+        store.addchat(payload.userId, user.name, payload.roomId, payload.message); 
+
+        const outgoingPayload = {
+            type: OutgoingSupportedMessages.AddChat,
+            payload : {
+                type: OutgoingSupportedMessages.AddChat,
+                payload :{
+                    roomId: payload.roomId,
+                    message: payload.message,
+                    name: user.name,
+                    upvote: 0
+                }
+            }
+        }
+    }
+    if ( message.type === SupportedMessage.UpvoteMessage) {
+        const payload = message.payload;
+        store.upvote(payload.userId, payload.roomId, payload.chatId);
+    }
+}
